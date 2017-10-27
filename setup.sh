30067301e73a76a66c4243f005b25fddd7ef78c0
pron.a <alexandre.pron@gmail.com>
2017-10-27 12:13:12 +0200

    Adding Prerequisites section in installation instructions

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Please enter the absolute location of your Brainvisa installation"
 read -p "Brainvisa installation location :" BV_path
 echo Diffuse will be setup up at this location $BV_path
 rsync -avz brainvisa/ $BV_path/brainvisa
-rsync -avz python/    $BV_path/brainvisa
-rsync -avz share/     $BV_path/brainvisa
+rsync -avz python/    $BV_path/python
+rsync -avz share/     $BV_path/share
 echo Diffuse has been succesfully copied into your Brainvisa directory
 echo Try lauching brainvisa with the --updateDocumentation option
